13d9dfa8e31f9d20f374ca60a036cc2ef81eabf9
Pavan Reddy Gottimukkula <46338498+pavanreddy21@users.noreply.github.com>
2022-12-22 10:27:41 -0500

    Update sync.bat

--- sync.bat
+++ sync.bat
@@ -7,9 +7,9 @@ set dst=E:\path\to\usb\folder
 rem Run the robocopy command
 robocopy %src% %dst% /e /mir
 
-rem If the robocopy command completes successfully, display a message
+rem Check the exit code of the robocopy command
 if %errorlevel% equ 0 (
     echo Sync completed successfully
 ) else (
-    echo Sync failed
+    echo Sync failed with error code %errorlevel%
 )
